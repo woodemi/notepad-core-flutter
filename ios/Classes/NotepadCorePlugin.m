390838e225be246c61da2e58388485ce521420f6
wangkun <wangkun@woodemi.com>
2019-11-26 21:38:26 +0800

    Remove sample code
    Signed-off-by: wangkun <wangkun@woodemi.com>

--- ios/Classes/NotepadCorePlugin.m
+++ ios/Classes/NotepadCorePlugin.m
@@ -1,20 +1,13 @@
 #import "NotepadCorePlugin.h"
 
 @implementation NotepadCorePlugin
-+ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  FlutterMethodChannel* channel = [FlutterMethodChannel
-      methodChannelWithName:@"notepad_core"
-            binaryMessenger:[registrar messenger]];
-  NotepadCorePlugin* instance = [[NotepadCorePlugin alloc] init];
-  [registrar addMethodCallDelegate:instance channel:channel];
++ (void)registerWithRegistrar:(NSObject <FlutterPluginRegistrar> *)registrar {
+    FlutterMethodChannel *channel = [FlutterMethodChannel methodChannelWithName:@"notepad_core" binaryMessenger:[registrar messenger]];
+    [registrar addMethodCallDelegate:[NotepadCorePlugin new] channel:channel];
 }
 
-- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
-  if ([@"getPlatformVersion" isEqualToString:call.method]) {
-    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
-  } else {
+- (void)handleMethodCall:(FlutterMethodCall *)call result:(FlutterResult)result {
     result(FlutterMethodNotImplemented);
-  }
 }
 
 @end
