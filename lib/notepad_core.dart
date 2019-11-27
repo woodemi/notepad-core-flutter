5e616bbe14978783e0e8a3e8af18a5b841b9091f
wangkun <wangkun@woodemi.com>
2019-11-28 07:05:14 +0800

    Extract `NotepadConnector` for initialization of message channel
    Signed-off-by: wangkun <wangkun@woodemi.com>

--- lib/notepad_core.dart
+++ lib/notepad_core.dart
@@ -7,31 +7,55 @@ export 'Notepad.dart';
 
 const _method = const MethodChannel('notepad_core/method');
 const _event_scanResult = const EventChannel('notepad_core/event.scanResult');
+const _message = BasicMessageChannel(
+    'notepad_core/message', StandardMessageCodec());
 
-void startScan() {
-  _method
-      .invokeMethod('startScan')
-      .then((_) => print('startScan invokeMethod success'));
-}
+final notepadConnector = NotepadConnector._();
 
-void stopScan() {
-  _method
-      .invokeMethod('stopScan')
-      .then((_) => print('stopScan invokeMethod success'));
-}
+class NotepadConnector {
+  NotepadConnector._() {
+    _message.setMessageHandler(_handleMessage);
+  }
 
-final Stream<NotepadScanResult> scanResultStream = _event_scanResult
-    .receiveBroadcastStream({'name': 'scanResult'})
-    .map((item) => NotepadScanResult.fromMap(item))
-    .where(support);
+  void startScan() {
+    _method
+        .invokeMethod('startScan')
+        .then((_) => print('startScan invokeMethod success'));
+  }
 
-void connect(NotepadScanResult scanResult) {
-  _method.invokeMethod('connect', {
-    'deviceId': scanResult.deviceId,
-  }).then((_) => print('connect invokeMethod success'));
-}
+  void stopScan() {
+    _method
+        .invokeMethod('stopScan')
+        .then((_) => print('stopScan invokeMethod success'));
+  }
 
-void disconnect() {
-  _method.invokeMethod('disconnect')
-      .then((_) => print('disconnect invokeMethod success'));
+  final Stream<NotepadScanResult> scanResultStream = _event_scanResult
+      .receiveBroadcastStream({'name': 'scanResult'})
+      .map((item) => NotepadScanResult.fromMap(item))
+      .where(support);
+
+  void connect(NotepadScanResult scanResult) {
+    _method.invokeMethod('connect', {
+      'deviceId': scanResult.deviceId,
+    }).then((_) => print('connect invokeMethod success'));
+  }
+
+  void disconnect() {
+    _method.invokeMethod('disconnect')
+        .then((_) => print('disconnect invokeMethod success'));
+  }
+
+  Future<dynamic> _handleMessage(dynamic message) async {
+    print('handleMessage $message');
+    var connectionState = message['ConnectionState'];
+    switch (connectionState) {
+      case 'Connected':
+        print('ConnectionState Connected');
+        break;
+      case 'Disconnected':
+      default:
+        print('ConnectionState Disconnected');
+        break;
+    }
+  }
 }
